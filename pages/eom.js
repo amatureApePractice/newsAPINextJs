bb88a629c5f81ac3d2974d16dcc1f9f5d138796c
drdarian <dariantaban@gmail.com>
2021-11-12 21:43:42 -0800

    dummy data and serverSideProps

--- pages/eom.js
+++ pages/eom.js
@@ -1,4 +1,5 @@
 export const EOM = ({ employee }) => {
+  console.log(employee);
   return (
     <div className='page-container'>
       <div>
@@ -9,7 +10,17 @@ export const EOM = ({ employee }) => {
 };
 
 export const getServerSideProps = async pageContext => {
+  const apiResponse = await fetch(
+    'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth',
+  )
 
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee
+    }
+  }
 };
 
 export default EOM;
