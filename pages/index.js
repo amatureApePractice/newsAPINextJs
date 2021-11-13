fd128d6d95058c6da54087f202e75f116e2a6ad0
drdarian <dariantaban@gmail.com>
2021-11-12 22:01:24 -0800

    building homepage

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className='page-container'>
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
+
+        <h3>Your one stop shop for the latest news articles.</h3>
+      </div>
+    </div>
   )
 }
