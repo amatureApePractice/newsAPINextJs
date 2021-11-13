cdece77060ff8977c4cd17f30f6e2cde0dfd9c4d
drdarian <dariantaban@gmail.com>
2021-11-12 22:34:03 -0800

    add toolbar to homepage

--- pages/eom.js
+++ pages/eom.js
@@ -1,9 +1,11 @@
 import styles from '../styles/EOM.module.css';
+import { Toolbar } from '../components/toolbar';
 
 export const EOM = ({ employee }) => {
   console.log(employee);
   return (
     <div className='page-container'>
+      <Toolbar />
       <div className={styles.main}>
         <h1>Employee Of The Month</h1>
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Toolbar } from '../components/toolbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className='page-container'>
+
+      <Toolbar />
       <div className={styles.main}>
         <h1>Next.js News App</h1>
 
